aa29359b94b1c52d1aaab20fa79957e968b5810f
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-05-23 16:28:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,30 @@ import sys
 
 extension_modules = []
 setup(
-    name="convnet",
+    name="striate",
     description="Convolutional neural networks in Python"
     long_description='''
-ConvNet 
+Striate. 
 =========
+A simple implementation of a Convolutional Neural Network which is based on (and extends) the [LeNet5 implementation](http://deeplearning.net/tutorial/lenet.html) in Theano's tutorial. 
+Though most of the code runs on the GPU (thanks Theano & PyCUDA!), it's still not nearly as fast as you could make it with hand-rolled kernels. Also, the provided training algorithm is just mini-batch 
+stochastic gradient descent. I'm using this library as a testbed for experimenting with other learning algorithms: the code is a sloppy prototype cobbled together from other examples and 
+the features are the minimum I need for my own needs. You've been warned! 
 
-Go talk to Yann LeCun. 
+* If you want top-notch runtime and accuracy, go talk to [Alex K](https://code.google.com/p/cuda-convnet/).
+
+* If you want to know about convolutional neural networks, go talk to [Yann LeCun](http://yann.lecun.com/). 
+
+**Requires**
+  
+  * CUDA
+  * Theano
+  * PyCUDA
+  * scikits.cuda 
+  * NumPy
+  * SciPy
+
+
 ''',
     classifiers=['Development Status :: 3 - Alpha',
 		 'Topic :: Scientific/Engineering :: Artificial Intelligence'                 
