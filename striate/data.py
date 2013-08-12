Fix off by one error in dataprovider.
The 'zero' epoch would consist of one batch prior to the first epoch
being run, which is a bit confusing.

striate/data.py
@@ -13,18 +13,11 @@ import sys
 import threading
 import time
 
-def load(filename):
+
+def _load(filename):
   with open(filename, 'rb') as f:
     d = cPickle.load(f)
   return d
-
-
-def join(lsts):
-  out = []
-  for l in lsts:
-    for item in l:
-      out.append(item)
-  return out
 
 
 BatchData = collections.namedtuple('BatchData',
@@ -39,13 +32,13 @@ class DataProvider(object):
     self.data_dir = data_dir
     self.meta_file = os.path.join(data_dir, 'batches.meta')
 
-    self.curr_batch_index = -1
+    self.curr_batch_index = 0
     self.curr_batch = None
-    self.curr_epoch = 0
+    self.curr_epoch = 1
     self.data = None
 
     if os.path.exists(self.meta_file):
-      self.batch_meta = load(self.meta_file)
+      self.batch_meta = _load(self.meta_file)
     else:
       print 'No default meta file \'batches.meta\', using another meta file'
 
@@ -71,15 +64,15 @@ class DataProvider(object):
   def get_batch_num(self):
     return len(self.batch_range)
 
-  @classmethod
-  def register_data_provider(cls, name, _class):
+  @staticmethod
+  def register_data_provider(name, _class):
     if name in dp_dict:
       print 'Data Provider', name, 'already registered'
     else:
       dp_dict[name] = _class
 
-  @classmethod
-  def get_by_name(cls, name):
+  @staticmethod
+  def get_by_name(name):
     if name not in dp_dict:
       print >> sys.stderr, 'There is no such data provider --', name, '--'
       sys.exit(-1)
@@ -138,7 +131,7 @@ class ImageNetDataProvider(ParallelDataProvider):
       cat_dirs = []
       for i in category_range:
         synid = self.batch_meta['label_to_synid'][i]
-        #util.log('Using category: %d, synid: %s, label: %s', i, synid, self.batch_meta['label_names'][i])
+        # util.log('Using category: %d, synid: %s, label: %s', i, synid, self.batch_meta['label_names'][i])
         cat_dirs.append(synid_to_dir[synid])
 
     self.images = []
@@ -171,7 +164,7 @@ class ImageNetDataProvider(ParallelDataProvider):
 
   def __trim_borders(self, images, target):
     for idx, img in enumerate(images):
-      #startY, startX = np.random.randint(0, self.border_size * 2 + 1), np.random.randint(0, self.border_size * 2 + 1)
+      # startY, startX = np.random.randint(0, self.border_size * 2 + 1), np.random.randint(0, self.border_size * 2 + 1)
       startY, startX = 0, 0
       endY, endX = startY + self.inner_size, startX + self.inner_size
       pic = img[:, startY:endY, startX:endX]
@@ -186,13 +179,14 @@ class ImageNetDataProvider(ParallelDataProvider):
     self.curr_batch = self.batch_range[self.curr_batch_index]
     if self.curr_batch_index == 0:
       self.curr_epoch += 1
+    
     epoch = self.curr_epoch
     batchnum = self.curr_batch
     names = self.images[self.batches[batchnum]]
     num_imgs = len(names)
     labels = np.zeros((1, num_imgs))
     cropped = np.ndarray((self.get_data_dims(), num_imgs * self.data_mult), dtype=np.uint8)
-    # load in parallel for training
+    # _load in parallel for training
     st = time.time()
     images = []
     for idx, filename in enumerate(names):
@@ -225,7 +219,7 @@ class ImageNetDataProvider(ParallelDataProvider):
     labels = labels.reshape(cropped.shape[1],)
     labels = np.require(labels, dtype=np.single, requirements='C')
 
-    #util.log("Loaded %d images in %.2f seconds (%.2f load, %.2f align)",
+    # util.log("Loaded %d images in %.2f seconds (%.2f _load, %.2f align)",
     #         num_imgs, time.time() - start, load_time, align_time)
     # self.data = {'data' : SharedArray(cropped), 'labels' : SharedArray(labels)}
     return BatchData(cropped, labels, epoch, batchnum)
@@ -234,14 +228,10 @@ class ImageNetDataProvider(ParallelDataProvider):
   # idx is the index of the matrix.
   def get_data_dims(self, idx=0):
     return self.inner_size ** 2 * 3 if idx == 0 else 1
-
-  def get_plottable_data(self, data):
-    return np.require(
-       (data + self.data_mean).T
-       .reshape(data.shape[1], 3, self.inner_size, self.inner_size)
-       .swapaxes(1, 3)
-       .swapaxes(1, 2) / 255.0,
-        dtype=np.single)
+  
+  @property
+  def image_shape(self):
+    return (3, self.inner_size, self.inner_size)
 
 
 class CifarDataProvider(DataProvider):
@@ -255,7 +245,7 @@ class CifarDataProvider(DataProvider):
 
     filename = os.path.join(self.data_dir, 'data_batch_%d' % self.curr_batch)
 
-    self.data = load(filename)
+    self.data = _load(filename)
     return BatchData(self.data['data'] - self.batch_meta['data_mean'],
                      np.array(self.data['labels']),
                      self.curr_epoch,
@@ -267,20 +257,24 @@ class CifarDataProvider(DataProvider):
   def get_batch_indexes(self):
     names = self.get_batch_filenames()
     return sorted(list(set(int(DataProvider.BATCH_REGEX.match(n).group(1)) for n in names)))
+  
+  @property
+  def image_shape(self):
+    return (3, 32, 32)
 
 
 
 class ImageNetCateGroupDataProvider(ImageNetDataProvider):
   TOTAL_CATEGORY = 1000
-  def __init__(self, data_dir, batch_range, num_group, batch_size = 128):
+  def __init__(self, data_dir, batch_range, num_group, batch_size=128):
     ImageNetDataProvider.__init__(self, data_dir, batch_range)
     self.num_group = num_group
 
   def _get_next_batch(self):
     data = ImageNetDataProvider._get_next_batch(self)
-    labels =  data.labels / (ImageNetCateGroupDataProvider.TOTAL_CATEGORY / self.num_group)
+    labels = data.labels / (ImageNetCateGroupDataProvider.TOTAL_CATEGORY / self.num_group)
     labels = labels.astype(np.int).astype(np.float)
-    return BatchData( data.data, labels, data.epoch, data.batchnum)
+    return BatchData(data.data, labels, data.epoch, data.batchnum)
 
 
 